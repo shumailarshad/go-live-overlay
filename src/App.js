7cb663a4c72d924fc64c5e567cf48932f0b2a30a
Shumail Arshad <shumailarshad@gmail.com>
2022-01-01 04:52:07 +0000

    Change play-cricket link to https

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
     const [liveScore, setLiveScore] = useState({});
 
     function updateLiveScore() {
-        axios.get(`http://play-cricket.com/api/v2/match_detail.json?match_id=5089096&api_token=496faf9d0375df628381f4f0d59eeb14`)
+        axios.get(`https://play-cricket.com/api/v2/match_detail.json?match_id=5089096&api_token=496faf9d0375df628381f4f0d59eeb14`)
             .then(res => {
                 const matchDetails = res.data['match_details'][0];
                 const firstInnings = matchDetails.innings[0];
@@ -28,21 +28,21 @@ function App() {
             })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         updateLiveScore();
-    },[])
+    }, [])
 
     return (
         <div className="App">
             <div className="AppMatchSummary Highlight">
             </div>
             <div className="AppMatchScore Highlight">
-                <div className="Highlight" style={{"flex": 1}}>
+                <div className="Highlight" style={{ "flex": 1 }}>
                     {liveScore.inningsScore} / {liveScore.inningsWicketsLost} for {liveScore.inningsOvers} overs
                 </div>
-                <div className="Highlight" style={{"flex": 1}}>
-                    {liveScore.onStrikeBatsmanName}: {liveScore.onStrikeBatsmanScore} ({liveScore.onStrikeBatsmanBallsFaced}) <br/>
-                    {liveScore.offStrikeBatsmanName}: {liveScore.offStrikeBatsmanScore} ({liveScore.offStrikeBatsmanBallsFaced}) <br/>
+                <div className="Highlight" style={{ "flex": 1 }}>
+                    {liveScore.onStrikeBatsmanName}: {liveScore.onStrikeBatsmanScore} ({liveScore.onStrikeBatsmanBallsFaced}) <br />
+                    {liveScore.offStrikeBatsmanName}: {liveScore.offStrikeBatsmanScore} ({liveScore.offStrikeBatsmanBallsFaced}) <br />
                 </div>
             </div>
         </div>
